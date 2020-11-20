3d9ba86866bbf95474a1a9d7f7c88f35b5008b40
Arcady1 <insite1029@gmail.com>
2020-11-20 19:21:25 +0300

    iris tracking

--- web/js/index.js
+++ web/js/index.js
@@ -1,6 +1,8 @@
-let canvas = document.getElementById("canvas");
+// ! let canvas = document.getElementById("canvas");
 let video = document.getElementById("video");
-let ctx = canvas.getContext("2d");
+let $irisDot = $("#iris-position-dot");
+let $video__wrapper = $("#video__wrapper");
+// ! let ctx = canvas.getContext("2d");
 
 navigator.getUserMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
@@ -25,8 +27,12 @@ navigator.mediaDevices.getUserMedia({
     })
     .then((model_) => {
         setInterval(() => {
-            modelPrediction(model_);
-        }, 1000);
+            modelPrediction(model_).then((predictions) => {
+                // ! console.log(predictions);
+                irisDotGenerator(predictions[0].annotations.leftEyeIris, predictions[0].annotations.rightEyeIris);
+                // ! irisDotPosition(predictions[0].annotations.leftEyeIris, predictions[0].annotations.rightEyeIris);
+            });
+        }, 13);
     })
     .catch(() => {
         console.log("ERROR: Sth wrong with modelPrediction()");
@@ -35,14 +41,44 @@ navigator.mediaDevices.getUserMedia({
 // Model loading
 async function modelLoading() {
     const model = await faceLandmarksDetection.load(
-        faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);
+        faceLandmarksDetection.SupportedPackages.mediapipe);
+    console.log("The model is loaded!");
     return model;
 }
 // Getting predictions; input: model 
 async function modelPrediction(model) {
     // an array of prediction objects for the faces in the input, which include information about each face
     const faces = await model.estimateFaces({
-        input: video
+        input: video,
+        // ! flipHorizontal: true
     });
-    console.log(faces);
+    return faces;
+    // console.log(faces);
+}
+// Iris dot changing position
+function irisDotPosition(leftIrisPos, rightIrisPos) {
+    console.log("left: ", leftIrisPos);
+    console.log("right: ", rightIrisPos);
+    console.log("==============");
+    $irisDot.css({
+        left: leftIrisPos[0][0] + "px",
+        top: leftIrisPos[0][1] + "px"
+    })
+}
+// Function circles the Iris; input: array of Iris position (x, y, z) 
+function irisDotGenerator(leftIrisPos, rightIrisPos) {
+    let xL, yL, xR, yR;
+
+    $(".iris-position-dot").remove();
+
+    for (let i = 0; i < leftIrisPos.length; i++) {
+        xL = leftIrisPos[i][0];
+        yL = leftIrisPos[i][1];
+        
+        xR = rightIrisPos[i][0];
+        yR = rightIrisPos[i][1];
+        
+        $video__wrapper.prepend('<div class="iris-position-dot" id="iris-position-dot" style="left: ' + xL + 'px; top: ' + yL + 'px"> </div>');
+        $video__wrapper.prepend('<div class="iris-position-dot" id="iris-position-dot" style="left: ' + xR + 'px; top: ' + yR + 'px"> </div>');
+    }
 }
