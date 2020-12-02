5d830eef9b79875d81c96c2676b79ed03f40a843
Arcady1 <insite1029@gmail.com>
2020-12-02 17:48:25 +0300

    blink-normal-Dist

--- web/js/face_points_render.js
+++ web/js/face_points_render.js
@@ -1,8 +1,9 @@
 // The function circles the face parts; input: array of face position (x, y, z) 
 function faceDotGenerator(...args) {
+    // CSS classes
+    let class__ = "iris-pos-dot ";
     // Removing previous points 
     $(".iris-pos-dot").remove();
-    let class__;
     // ? Face rendering
     // ? args.forEach((facePart, index) => { // Part of face anf index from "faceDotGenerator"
     // ?     let class_ = "iris-pos-dot";
@@ -27,7 +28,7 @@ function faceDotGenerator(...args) {
         },
         // Left eye
         "leftLowerEyePos": {
-            "x": min(args[1][4][0], args[1][3][0]) + (Math.abs(args[1][4][0] - args[1][3][0]) / 2),
+            // We don't need X, because we use irisLeft X
             "y": min(args[1][4][1], args[1][3][1]) + (Math.abs(args[1][4][1] - args[1][3][1]) / 2)
         },
         "leftUpperEyePos": {
@@ -36,32 +37,31 @@ function faceDotGenerator(...args) {
         },
         // Right eye
         "rightLowerEyePos": {
-            "x": min(args[4][4][0], args[4][3][0]) + (Math.abs(args[4][4][0] - args[4][3][0]) / 2),
+            // We don't need X, because we use irisLeft X
             "y": min(args[4][4][1], args[4][3][1]) + (Math.abs(args[4][4][1] - args[4][3][1]) / 2)
         },
         "rightUpperEyePos": {
             "x": args[5][3][0],
             "y": args[5][3][1]
         }
-    }
-
-    class__ = "iris-pos-dot ";
+    }    
     // Irises
     facePointsRendering(faceParts.irisLeft.x, faceParts.irisLeft.y, class__);
     facePointsRendering(faceParts.irisRight.x, faceParts.irisRight.y, class__);
     // Left eye
-    facePointsRendering(faceParts.leftLowerEyePos.x, faceParts.leftLowerEyePos.y, class__ + "eyes-red-style");
-    facePointsRendering(faceParts.leftUpperEyePos.x, faceParts.leftUpperEyePos.y, class__ + "eyes-blue-style");
+    facePointsRendering(faceParts.irisLeft.x, faceParts.leftLowerEyePos.y, class__ + "eyes-red-style");
+    facePointsRendering(faceParts.irisLeft.x, faceParts.leftUpperEyePos.y, class__ + "eyes-blue-style");
     // Right eye
-    facePointsRendering(faceParts.rightLowerEyePos.x, faceParts.rightLowerEyePos.y, class__ + "eyes-red-style");
-    facePointsRendering(faceParts.rightUpperEyePos.x, faceParts.rightUpperEyePos.y, class__ + "eyes-blue-style");
-
+    facePointsRendering(faceParts.irisRight.x, faceParts.rightLowerEyePos.y, class__ + "eyes-red-style");
+    facePointsRendering(faceParts.irisRight.x, faceParts.rightUpperEyePos.y, class__ + "eyes-blue-style");
 
     // Setting currentDist
     currentDist = {
-        "leftEyeYDist": faceParts.leftUpperEyePos.y - faceParts.leftLowerEyePos.y,
-        "RightEyeYDist": faceParts.rightUpperEyePos.y - faceParts.rightLowerEyePos.y
+        "leftEyeYDist": Math.abs(faceParts.leftUpperEyePos.y - faceParts.leftLowerEyePos.y),
+        "rightEyeYDist": Math.abs(faceParts.rightUpperEyePos.y - faceParts.rightLowerEyePos.y)
     };
+
+    // ! console.log(currentDist.leftEyeYDist, currentDist.rightEyeYDist);
     // Setting normalDist
     if (setNormalDist == true) {
         normalDist = currentDist;
