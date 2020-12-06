95e206b4e4305de7e512bc39ccb3683a38450b2d
Arcady1 <insite1029@gmail.com>
2020-12-06 23:36:41 +0300

    dist between Eye and Iris

--- web/js/scroll.js
+++ web/js/scroll.js
@@ -1,4 +1,4 @@
-function makeScroll() {
+function makeScroll(faceParts) {
     console.log(normDistBetweenEyeCenterAndIrisCenter, faceParts.midwayBetweenEyes.y - faceParts.midwayBetweenIrises.y);
     // TODO Проверяем сдвиг по оси OY текущего положения зрачка относитнльно нормального
     // TODO Делаем скролл в нужном напрвлении
