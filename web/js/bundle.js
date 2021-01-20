a717dcaf3334d6168dec3e6cf81c5a541eb3e073
Arcady1 <insite1029@gmail.com>
2021-01-20 20:51:16 +0300

    line-chart-data-start

--- web/js/bundle.js
+++ web/js/bundle.js
@@ -32844,36 +32844,45 @@ function printChart() {
     let ctx = canvas.getContext("2d");
     let myChart;
 
-    return function() {
+    return function () {
         myChart = new Chart(ctx, {
-            type: 'bar',
+            type: 'line',
             data: {
                 labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                 datasets: [{
-                    label: '# of Votes',
                     data: [12, 19, 3, 5, 2, 3],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'transparent'
                     ],
                     borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        '#fff'
                     ],
                     borderWidth: 1
                 }]
             },
             options: {
+                title: {
+                    display: true,
+                    fontColor: '#fff',
+                    text: "TITLE"
+                },
                 scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            fontColor: '#fff',
+                            display: true,
+                            labelString: "XXX"
+                        },
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
                     yAxes: [{
+                        scaleLabel: {
+                            fontColor: '#fff',
+                            display: true,
+                            labelString: "YYY"
+                        },
                         ticks: {
                             beginAtZero: true
                         }
